aed44f8acc59f84bc8b1983530f69f605927487c
shmat00ff <shmat00ff@gmail.com>
2025-01-16 15:11:51 +0700

    Some fixes

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, String, Integer, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = 'sqlite:///chat_history.db'  # Choose your own db address
+DATABASE_URL = 'sqlite:///chat_history.db'
 engine = create_engine(DATABASE_URL)
 
 Base = declarative_base()
